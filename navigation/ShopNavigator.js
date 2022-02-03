b7b03c1d44bb35a5b0b40a5052864c9c25310ad8
Sathesh <satheshmgn21@gmail.com>
2022-02-03 18:15:10 +0530

    redux

---
+++ navigation/ShopNavigator.js
@@ -0,0 +1,28 @@
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from "react-navigation"
+
+import { Platform } from 'react-native';
+import ProductsOverviewscreen from "../screens/shop/ProductsOverviewScreen"
+import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
+import Colors from '../constants/Colors';
+import CartScreen from '../screens/shop/CartScreen';
+
+const ProductsNavigator = createStackNavigator({
+    ProductsOverview: ProductsOverviewscreen,
+    ProductDetail: ProductDetailScreen,
+    Cart : CartScreen
+}, {
+    defaultNavigationOptions: {
+        headerStyle: {
+            backgroundColor: Platform.OS === "android" ? Colors.primary : ""
+        },
+        headerTitleStyle: {
+            fontFamily: "open-sans-bold"
+        }, headerBackTitleStyle: {
+            fontFamily: "open-sans"
+        },
+        headerTintColor: Platform.OS === "android" ? "white" : Colors.primary
+    }
+})
+
+export default createAppContainer(ProductsNavigator);
